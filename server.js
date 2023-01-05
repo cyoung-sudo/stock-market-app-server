cab522c456fa60c6f98077d3260f5f5ec213c6ad
Christopher Young <christopheryoung@MacBook123.local>
2023-01-05 12:31:14 -0800

    socket setup

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 const db = require("./db/conn");
 const helmet = require("helmet");
+// Socket
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
 //---- Middleware
 app.use(cors());
@@ -14,9 +19,14 @@ app.use(helmet());
 
 //----- Routes
 app.use(require("./routes/stockData"));
+
+//----- Socket events
+io.on("connection", socket => {
+  console.log(`New connected: ${socket.id}`);
+});
  
 //----- Connection
-app.listen(port, () => {
+httpServer.listen(port, () => {
   db.connect(); // Connect to DB
   console.log(`Server is running on port: ${port}`);
 });
